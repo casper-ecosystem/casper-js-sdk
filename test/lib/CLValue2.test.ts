b3aed28772bd92641c0a8e7c9d7d3dff28c24d4a
Jan Hoffmann <jan@hfmn.pl>
2021-03-12 12:03:51 +0100

    full test coverage

--- test/lib/CLValue2.test.ts
+++ test/lib/CLValue2.test.ts
@@ -2,10 +2,36 @@ import { expect } from 'chai';
 import { List, Bool } from '../../src/lib/CLValue2';
 
 describe('New Implementation of CLValue.list', () => {
+  it('Bool should return proper clType', () => {
+    const myBool = new Bool(false);
+    const clType = myBool.clType();
+    expect(clType.toString()).to.be.eq("Bool");
+  });
+
+  it('List should return proper clType', () => {
+    const myBool = new Bool(false);
+    const myList = new List<Bool>([myBool]);
+
+    expect(myList.clType().toString()).to.be.eq("List (Bool)");
+  });
+
   it('Should be able to create List with proper values - correct by construction', () => {
     const myList = new List([new Bool(true), new Bool(false)]);
 
     expect(myList).to.be.an.instanceof(List);
+  });
+
+  it('Should be able to return proper values by calling .value() on List', () => {
+    const myBool = new Bool(false);
+    const myList = new List<Bool>([myBool]);
+
+    expect(myList.value()).to.be.deep.eq([myBool]);
+  });
+
+  it('Should be able to return proper values by calling .value() on Bool', () => {
+    const myBool = new Bool(false);
+
+    expect(myBool.value()).to.be.eq(false);
   });
 
   // NOTE: Irrelevant in TS
@@ -18,44 +44,57 @@ describe('New Implementation of CLValue.list', () => {
   it('Should able to create empty List by providing type', () => {
     const mList = new List<Bool>();
     const len = mList.size();
+
     expect(len).to.equal(0);
   });
 
   it('Get should return proper value', () => {
     const myList = new List([new Bool(true)]);
     const newItem = new Bool(false);
+
     myList.push(newItem);
+
     expect(myList.get(1)).to.deep.eq(newItem);
   });
 
   it('Set should be able to push at current index + 1', () => {
     const myList = new List([new Bool(true)]);
     const newItem = new Bool(false);
+
     myList.set(0, newItem);
+
     expect(myList.get(0)).to.deep.eq(newItem);
   });
 
   it('Push should be consistent with types', () => {
     const myList = new List([new Bool(true)]);
+
     myList.push(new Bool(false));
+
     expect(myList.size()).to.equal(2);
   });
 
   it('Pop should remove last item from array', () => {
     const myList = new List([new Bool(true), new Bool(false)]);
+
     myList.pop();
+
     expect(myList.size()).to.equal(1);
   });
 
   it('Should set nested value by chaining methods', () => {
     const myList = new List([new List([new Bool(true), new Bool(false)])]);
+
     myList.get(0).set(1, new Bool(true));
+
     expect(myList.get(0).get(1)).to.deep.eq(new Bool(true));
   });
 
   it('Remove should remove item at certein index', () => {
     const myList = new List([new Bool(true), new Bool(false)]);
+
     myList.remove(0);
+
     expect(myList.get(0)).to.deep.eq(new Bool(false));
   });
 });
