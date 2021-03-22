9793c516c9a570e058976d5134317f058c6b018d
Jan Hoffmann <jan@hfmn.pl>
2021-03-22 13:37:30 +0100

    CLMap is now based on map

--- src/lib/CLValue2/Map.test.ts
+++ src/lib/CLValue2/Map.test.ts
@@ -1,28 +1,28 @@
 import { expect } from 'chai';
 import { CLValue } from './Abstract';
-import { MapValue } from './Map';
+import { CLMap } from './Map';
 import { CLBool } from './Bool';
 import { CLString, CLStringType } from './String';
 import { CLI32 } from './Numeric';
 
-describe('CLValue MapValue implementation', () => {
-  it('Maps hould return proper clType', () => {
-    const myMap = new MapValue([[new CLBool(true), new CLBool(false)]]);
+describe('CLValue CLMap implementation', () => {
+  it('Maps should return proper clType', () => {
+    const myMap = new CLMap([[new CLBool(true), new CLBool(false)]]);
 
     expect(myMap.clType().toString()).to.be.eq('Map (Bool: Bool)');
   });
 
   it('Should be able to create Map with proper values - correct by construction', () => {
     const inside: [CLValue, CLValue] = [new CLString('ABC'), new CLI32(123)]
-    const myMap = new MapValue([inside]);
+    const myMap = new CLMap([inside]);
 
-    expect(myMap).to.be.an.instanceof(MapValue);
-    expect(myMap.value()).to.be.deep.eq([inside]);
+    expect(myMap).to.be.an.instanceof(CLMap);
+    expect(myMap.value()).to.be.deep.eq(new Map([inside]));
   });
 
-  it('Should throw an error when MapValue is not correct by construction', () => {
+  it('Should throw an error when CLMap is not correct by construction', () => {
     const badFn = () =>
-      new MapValue([
+      new CLMap([
         [new CLString('ABC'), new CLI32(123)],
         [new CLString('DEF'), new CLBool(false)]
       ]);
@@ -30,10 +30,10 @@ describe('CLValue MapValue implementation', () => {
     expect(badFn).to.throw('Invalid data provided.');
   });
 
-  it('Should throw an error when MapValue is not correct by construction', () => {
+  it('Should throw an error when CLMap is not correct by construction', () => {
     const badFn = () =>
       // @ts-ignore
-      new MapValue([
+      new CLMap([
         [new CLString('ABC'), 2]
       ]);
 
@@ -43,7 +43,7 @@ describe('CLValue MapValue implementation', () => {
   it('Should be able to return proper values by calling .get() on Map', () => {
     const myKey = new CLString('ABC');
     const myVal = new CLI32(10);
-    const myMap = new MapValue([[myKey, myVal ]]);
+    const myMap = new CLMap([[myKey, myVal ]]);
 
     expect(myMap.get(myKey).value()).to.be.deep.eq(myVal.value());
   });
@@ -51,13 +51,13 @@ describe('CLValue MapValue implementation', () => {
   it('Get() should return indefined on non-existing key', () => {
     const myKey = new CLString('ABC');
     const myVal = new CLI32(10);
-    const myMap = new MapValue([[myKey, myVal ]]);
+    const myMap = new CLMap([[myKey, myVal ]]);
 
     expect(myMap.get(new CLString("DEF"))).to.be.deep.eq(undefined);
   });
 
   it('Should able to create empty Map by providing type', () => {
-    const myMap = new MapValue([
+    const myMap = new CLMap([
       new CLStringType(),
       new CLStringType()
     ]);
@@ -69,7 +69,7 @@ describe('CLValue MapValue implementation', () => {
   it('Set should be able to set values at already declared keys', () => {
     const myKey = new CLString('ABC');
     const myVal = new CLI32(10);
-    const myMap = new MapValue([[myKey, myVal ]]);
+    const myMap = new CLMap([[myKey, myVal ]]);
     const newVal = new CLI32(11);
 
     myMap.set(myKey, newVal);
@@ -80,7 +80,7 @@ describe('CLValue MapValue implementation', () => {
   it('Set should be able to set values at already declared keys', () => {
     const myKey = new CLString('ABC');
     const myVal = new CLI32(10);
-    const myMap = new MapValue([[myKey, myVal ]]);
+    const myMap = new CLMap([[myKey, myVal ]]);
     const newVal = new CLI32(11);
 
     myMap.set(myKey, newVal);
@@ -92,7 +92,7 @@ describe('CLValue MapValue implementation', () => {
   it('Set should be able to set values at empty keys', () => {
     const myKey = new CLString('ABC');
     const myVal = new CLI32(10);
-    const myMap = new MapValue([[myKey, myVal ]]);
+    const myMap = new CLMap([[myKey, myVal ]]);
     const newKey = new CLString("DEF");
     const newVal = new CLI32(11);
 
@@ -105,7 +105,7 @@ describe('CLValue MapValue implementation', () => {
   it('Remove should remove key/value pair at already declared keys', () => {
     const myKey = new CLString('ABC');
     const myVal = new CLI32(10);
-    const myMap = new MapValue([[myKey, myVal ]]);
+    const myMap = new CLMap([[myKey, myVal ]]);
 
     myMap.delete(myKey);
 
--- src/lib/CLValue2/Map.ts
+++ src/lib/CLValue2/Map.ts
@@ -1,6 +1,6 @@
 import { CLType, CLValue } from './Abstract';
 
-export class MapValueType<K extends CLType, V extends CLType> extends CLType {
+export class CLMapType<K extends CLType, V extends CLType> extends CLType {
   innerKey: K;
   innerValue: V;
 
@@ -20,25 +20,20 @@ export interface MapEntryType {
   value: CLType;
 }
 
-export interface MapEntryValue {
-  key: CLType;
-  value: CLType;
-}
-
 const isValueConstructor = (
-  v: Array<[CLValue, CLValue]> | [CLType, CLType]
-): v is Array<[CLValue, CLValue]> => {
-  return (
-    Array.isArray(v) &&
-    Array.isArray(v[0]) &&
-    v[0].length === 2 &&
-    !!v[0][0].clType &&
-    !!v[0][1].clType
-  );
-};
+   v: Array<[CLValue, CLValue]> | [CLType, CLType]
+ ): v is Array<[CLValue, CLValue]> => {
+   return (
+     Array.isArray(v) &&
+     Array.isArray(v[0]) &&
+     v[0].length === 2 &&
+     !!v[0][0].clType &&
+     !!v[0][1].clType
+   );
+ };
 
-export class MapValue<K extends CLValue, V extends CLValue> extends CLValue {
-  data: Array<[K, V]>;
+export class CLMap<K extends CLValue, V extends CLValue> extends CLValue {
+  data: Map<K, V>;
   refType: [CLType, CLType];
   /**
    * Constructs a new `MapValue`.
@@ -48,7 +43,6 @@ export class MapValue<K extends CLValue, V extends CLValue> extends CLValue {
   constructor(v: [K, V][] | [CLType, CLType]) {
     super();
     if (isValueConstructor(v)) {
-      // const refType = v[0].clType();
       this.refType = [v[0][0].clType(), v[0][1].clType()];
       if (
         v.every(([key, value]) => {
@@ -58,46 +52,39 @@ export class MapValue<K extends CLValue, V extends CLValue> extends CLValue {
           );
         })
       ) {
-        this.data = v;
+        this.data = new Map(v);
       } else {
         throw Error('Invalid data provided.');
       }
     } else if (v[0] instanceof CLType && v[1] instanceof CLType) {
       this.refType = v;
-      this.data = [];
+      this.data = new Map();
     } else {
       throw Error('Invalid data type(s) provided.');
     }
   }
 
   clType(): CLType {
-    return new MapValueType(this.data[0][0].clType(), this.data[0][1].clType());
+    return new CLMapType(...this.refType);
   }
 
-  value(): Array<[K, V]> {
+  value(): Map<K, V> {
     return this.data;
   }
 
   get(k: K): V | undefined {
-    const found = this.data.find(v => k === v[0]);
-    if (found) return found[1];
-    return undefined;
+    return this.data.get(k);
   }
 
   set(k: K, val: V): void {
-    const foundIdx = this.data.findIndex(v => k === v[0]);
-    if (foundIdx > -1) {
-      this.data[foundIdx] = [k, val];
-      return;
-    }
-    this.data.push([k, val]);
+    this.data.set(k, val);
   }
 
-  delete(k: K): void {
-    this.data = this.data.filter(([key]) => key !== k);
+  delete(k: K): boolean {
+    return this.data.delete(k);
   }
 
   size(): number {
-    return this.data.length;
+    return this.data.size;
   }
 }
