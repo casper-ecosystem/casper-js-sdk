135a13aaacee9584b8afdde38633cfa42e971a77
Jan Hoffmann <jan@hfmn.pl>
2021-03-25 18:47:50 +0100

    base result implementation

---
+++ src/lib/CLValue2/Result.ts
@@ -0,0 +1,76 @@
+import { concat } from '@ethersproject/bytes';
+import { Ok, Err, Result } from 'ts-results';
+
+import { CLType, CLValue, ToBytes } from './Abstract';
+import { toBytesU8 } from '../ByteConverters';
+
+export enum CLErrorCodes {
+  EarlyEndOfStream = 0,
+  Formatting,
+  LeftOverBytes,
+  OutOfMemory
+}
+
+export class CLErrorType extends CLType {
+  toString(): string {
+    return 'Error';
+  }
+}
+
+/**
+ * Class representing a result of an operation that might have failed. Can contain either a value
+ * resulting from a successful completion of a calculation, or an error. Similar to `Result` in Rust
+ * or `Either` in Haskell.
+ */
+export class CLResultType extends CLType {
+  toString(): string {
+    return 'Result';
+  }
+}
+
+export class CLResult<T extends CLValue & ToBytes, E extends CLErrorCodes>
+  extends CLValue
+  implements ToBytes {
+  public data: T | null;
+  public error: E | null;
+
+  constructor(data: T | null, error: E | null) {
+    super();
+    if (data) this.data = data;
+    if (error) this.error = error;
+  }
+
+  clType(): CLType {
+    return new CLResultType();
+  }
+
+  value(): Result<T, E> {
+    if (this.data) {
+      return Ok(this.data);
+    } else if (this.error) {
+      return Err(this.error);
+    }
+  }
+
+  public static OK<T extends CLValue, E extends CLErrorCodes>(
+    val: T
+  ): CLResult<T, E> {
+    return new CLResult<T, E>(val, null);
+  }
+
+  public static Error<T extends CLValue, E extends CLErrorCodes>(
+    err: E
+  ): CLResult<T, E> {
+    return new CLResult<T, E>(null, err);
+  }
+
+  toBytes(): Uint8Array {
+    if (this.data) {
+      return concat([toBytesU8(1), this.data.toBytes()]);
+    } else if (this.error) {
+      return concat([toBytesU8(0), toBytesU8(this.error)]);
+    } else {
+      throw new Error('Missing proper data');
+    }
+  }
+}
