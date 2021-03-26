11f64a35b6c2109e519718722f208ddb34055e2d
Jan Hoffmann <jan@hfmn.pl>
2021-03-26 11:44:44 +0100

    ByteArray max-length added

--- src/lib/CLValue2/ByteArray.ts
+++ src/lib/CLValue2/ByteArray.ts
@@ -1,5 +1,7 @@
 import { CLType, CLValue, ToBytes } from './Abstract';
 
+export const CL_BYTE_ARRAY_MAX_LENGTH = 32;
+
 export class CLByteArrayType extends CLType {
   toString(): string {
     return 'ByteArray';
@@ -9,12 +11,15 @@ export class CLByteArrayType extends CLType {
 export class CLByteArray extends CLValue implements ToBytes {
   data: Uint8Array;
   /**
-   * Constructs a new `ByteArrayValue`.
+   * Constructs a new `CLByteArray`.
    *
-   * @param v The bytes. 
+   * @param v The bytes array with max length 32.
    */
   constructor(v: Uint8Array) {
     super();
+    if (v.length > CL_BYTE_ARRAY_MAX_LENGTH) {
+      throw new Error(`Provided value has length ${v.length} which exceeded the limit (${CL_BYTE_ARRAY_MAX_LENGTH})`)
+    }
     this.data = v;
   }
 
