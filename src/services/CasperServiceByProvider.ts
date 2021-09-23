5da6d6edc4b30eaf8215515d7734f0a4832b9484
chaitanyapotti <chaitanya.potti@gmail.com>
2021-09-23 14:43:26 +0800

    Move files

--- src/services/CasperServiceByProvider.ts
+++ src/services/CasperServiceByProvider.ts
@@ -1,11 +1,12 @@
 import Client, { RequestManager } from '@open-rpc/client-js';
 import ProviderTransport, {
   SafeEventEmitterProvider
-} from '../lib/ProviderTransport';
+} from './ProviderTransport';
 import { CasperServiceByJsonRPC } from './CasperServiceByJsonRPC';
 
 export class CasperServiceByProvider extends CasperServiceByJsonRPC {
   constructor(provider: SafeEventEmitterProvider) {
+    // Hack to override base class
     super('https://example.com');
     const transport = new ProviderTransport(provider);
     const requestManager = new RequestManager([transport]);
