d8753d148e466340f500fcdcb024ebcee8e99c65
Dylan Ireland <dylan@casper.network>
2022-08-14 23:35:08 -0400

    Signer documented

--- src/lib/Signer.ts
+++ src/lib/Signer.ts
@@ -18,12 +18,18 @@ declare global {
   }
 }
 
+/**
+ * Can be used to determine whether the CasperLabs Helper is available at `window`
+ * @returns `true` if the CasperLabs Helper is present, `false` otherwise
+ */
 const helperPresent = () => {
   return !(typeof window.casperlabsHelper === 'undefined');
 };
 
 /**
- * Returns Signer version
+ * Gets the current Signer version
+ * @returns The current Signer version
+ * @throws `Error` if the JavaScript client isn't connected to the CasperLabs Signer browser extension
  */
 export const getVersion: () => Promise<string> = async () => {
   if (helperPresent()) {
@@ -41,7 +47,9 @@ export const getVersion: () => Promise<string> = async () => {
 };
 
 /**
- * Check whether CasperLabs Signer extension is connected
+ * Checks whether CasperLabs Signer extension is connected
+ * @returns `true` if the Signer is connected to the current website
+ * @throws `Error` if the JavaScript client isn't connected to the CasperLabs Signer browser extension
  */
 export const isConnected: () => Promise<boolean> = async () => {
   if (helperPresent()) return await window.casperlabsHelper.isConnected();
@@ -54,6 +62,7 @@ export const isConnected: () => Promise<boolean> = async () => {
 
 /**
  * Attempt connection to Signer
+ * @throws `Error` if the JavaScript client isn't connected to the CasperLabs Signer browser extension
  */
 export const sendConnectionRequest: () => void = () => {
   if (helperPresent()) return window.casperlabsHelper.requestConnection();
@@ -63,10 +72,11 @@ export const sendConnectionRequest: () => void = () => {
 };
 
 /**
- * **Deprecated** in favour of `getActivePublicKey()`.
- * Returns `base64` encoded public key of currently selected account.
+ * Gets the public key currently selected in the Signer in base64 format
+ * @deprecated in favour of {@link Signer.getActivePublicKey}.
+ * @returns `Promise` that resolves to a `base64` encoded public key string of the currently selected account
  *
- * @throws Error if haven't connected to CasperLabs Signer browser extension.
+ * @throws `Error` if the JavaScript client isn't connected to the CasperLabs Signer browser extension
  */
 export const getSelectedPublicKeyBase64: () => Promise<string> = () => {
   if (helperPresent())
@@ -79,7 +89,8 @@ export const getSelectedPublicKeyBase64: () => Promise<string> = () => {
 /**
  * Retrieve the active public key.
  *
- * @returns {string} Hex-encoded public key with algorithm prefix.
+ * @returns {string} Hex-encoded public key with algorithm prefix, where 01 indicates an Ed25519 key and 02 indicates Secp256k1
+ * @throws `Error` if the JavaScript client isn't connected to the CasperLabs Signer browser extension
  */
 export const getActivePublicKey: () => Promise<string> = () => {
   if (helperPresent()) return window.casperlabsHelper.getActivePublicKey();
@@ -93,10 +104,12 @@ export const getActivePublicKey: () => Promise<string> = () => {
 /**
  * Send Deploy in JSON format to Signer extension to be signed.
  *
- * @param deploy - deploy in JSON format
- * @param signingPublicKeyHex - Hex-formatted public key. The corresponding secret key is used to sign the deploy.
- * @param {string} [targetPublicKeyHex] - Hex-formatted public key.
+ * @param deploy A deploy in JSON format
+ * @param signingPublicKeyHex Hex-formatted public key. The corresponding secret key is used to sign the deploy.
+ * @param {string} targetPublicKeyHex Hex-formatted target public key.
  * If the `target` in the deploy is an account hash this can be used to verify it and display the hex-formatted public key in the UI.
+ *
+ * @returns A signed JSON deploy object
  *
  * @throws Errors if the Signer extension is not connected.
  * @throws Errors if signingPublicKeyHex is not available or does not match the Active Key in the Signer.
@@ -124,6 +137,13 @@ export const sign: (
   );
 };
 
+/**
+ * Initiates a signature request with the Signer. Used for signing messages
+ * @param {string} message The message to sign
+ * @param {string} signingPublicKey The public key of the signing account
+ * @returns A `Promise` that resolves to the signed message
+ * @throws `Error` if the JavaScript client isn't connected to the CasperLabs Signer browser extension
+ */
 export const signMessage: (
   message: string,
   signingPublicKey: string
@@ -137,8 +157,9 @@ export const signMessage: (
   );
 };
 
-/*
- * Forces Signer to disconnect from the currently open site.
+/**
+ * Disconnects from the currently open site
+ * @throws `Error` if the JavaScript client isn't connected to the CasperLabs Signer browser extension
  */
 export const disconnectFromSite: () => void = () => {
   if (helperPresent()) return window.casperlabsHelper.disconnectFromSite();
@@ -149,6 +170,10 @@ export const disconnectFromSite: () => void = () => {
   );
 };
 
+/**
+ * Forces Signer to connect to the currently open site
+ * @throws `Error` if the Signer isn't available to the JavaScript client
+ */
 export const forceConnection: () => void = () => {
   if (helperPresent()) return window.signerTestingHelper.forceConnection();
   return Promise.reject(
@@ -158,6 +183,10 @@ export const forceConnection: () => void = () => {
   );
 };
 
+/**
+ * Forces Signer to disconnect from the currently open site
+ * @throws `Error` if the JavaScript client isn't connected to the CasperLabs Signer browser extension
+ */
 export const forceDisconnect: () => void = () => {
   if (helperPresent()) return window.signerTestingHelper.forceDisconnect();
   return Promise.reject(
@@ -167,6 +196,11 @@ export const forceDisconnect: () => void = () => {
   );
 };
 
+/**
+ * Query the Signer to check if the user has created a vault
+ * @returns A `Promise` that resolves to a `boolean` indicating whether the user has or hasn't created a vault
+ * @throws `Error` if the JavaScript client isn't connected to the CasperLabs Signer browser extension
+ */
 export const hasCreatedVault: () => Promise<boolean> = () => {
   if (helperPresent()) return window.signerTestingHelper.hasCreatedVault();
   return Promise.reject(
@@ -176,6 +210,11 @@ export const hasCreatedVault: () => Promise<boolean> = () => {
   );
 };
 
+/**
+ * Initiates a request to reset the user's existing vault
+ * @returns A `Promise` that resolves to `void`
+ * @throws `Error` if the JavaScript client isn't connected to the CasperLabs Signer browser extension
+ */
 export const resetExistingVault: () => Promise<void> = () => {
   if (helperPresent()) return window.signerTestingHelper.resetExistingVault();
   return Promise.reject(
@@ -185,6 +224,12 @@ export const resetExistingVault: () => Promise<void> = () => {
   );
 };
 
+/**
+ * Initiates a request to create a new vault within the Signer
+ * @param {string} password The password of the new vault
+ * @returns A `Promise` that resolves to `void`
+ * @throws `Error` if the JavaScript client isn't connected to the CasperLabs Signer browser extension
+ */
 export const createNewVault: (password: string) => Promise<void> = (
   password: string
 ) => {
@@ -197,6 +242,13 @@ export const createNewVault: (password: string) => Promise<void> = (
   );
 };
 
+/**
+ * Initiates a request to create a test account within the Signer
+ * @param {string} name The name of the test account
+ * @param {string} privateKey The private key of the new test account
+ * @returns A `Promise` that resolves to `void`
+ * @throws `Error` if the JavaScript client isn't connected to the CasperLabs Signer browser extension
+ */
 export const createTestAccount: (
   name: string,
   privateKey: string
@@ -210,6 +262,12 @@ export const createTestAccount: (
   );
 };
 
+/**
+ * Gets the current transaction nonce
+ * @returns A `Promise` that resolves to the transaction nonce, or `null`
+ * @throws `Error` if the JavaScript client isn't connected to the CasperLabs Signer browser extension
+ * @privateRemarks Documentation needs reviewing
+ */
 export const getToSignMessageID: () => Promise<number | null> = () => {
   if (helperPresent()) return window.signerTestingHelper.getToSignMessageID();
   return Promise.reject(
@@ -219,6 +277,13 @@ export const getToSignMessageID: () => Promise<number | null> = () => {
   );
 };
 
+/**
+ * Sign a test deploy with a given id (nonce)
+ * @param {number} msgId The transaction id
+ * @returns A `Promise` that resolves to `void`
+ * @throws `Error` if the JavaScript client isn't connected to the CasperLabs Signer browser extension
+ * @privateRemarks Documentation needs reviewing
+ */
 export const signTestDeploy: (msgId: number) => Promise<void> = (
   msgId: number
 ) => {
