8e61df9feec6d35d18e47ef80618b1e86fb10722
gyroflaw <83718263+gyroflaw@users.noreply.github.com>
2023-02-27 20:53:26 +0900

    chore(deps): updated typedoc related modules and script

---
+++ typedoc.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('typedoc').TypeDocOptions} */
+module.exports = {
+  entryPoints: ['./src/index.ts'],
+  out: 'docs',
+  navigationLinks: {
+    GitHub: 'https://github.com/casper-ecosystem/casper-js-sdk'
+  }
+};
