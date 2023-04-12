df5f099bd44690e759459d51d23cf015536b5abb
gyroflaw <83718263+gyroflaw@users.noreply.github.com>
2023-04-12 22:10:53 +0900

    add test case with test vectors from slip 0010

--- src/lib/CasperHDKeys/Ed25519HDKey.test.ts
+++ src/lib/CasperHDKeys/Ed25519HDKey.test.ts
@@ -3,12 +3,105 @@ import { Ed25519HDKey } from './Ed25519HDKey';
 import { decodeBase16, encodeBase16 } from '../Conversions';
 
 describe('Ed25519HDKey', () => {
-  // Test vectors: https://github.com/satoshilabs/slips/blob/master/slip-0010.md
-  const vector1 = decodeBase16('000102030405060708090a0b0c0d0e0f');
-  const vector2 = decodeBase16(
-    'fffcf9f6f3f0edeae7e4e1dedbd8d5d2cfccc9c6c3c0bdbab7b4b1aeaba8a5a29f9c999693908d8a8784817e7b7875726f6c696663605d5a5754514e4b484542'
-  );
-
+  // https://github.com/satoshilabs/slips/blob/master/slip-0010.md#test-vector-1-for-ed25519
+  // https://github.com/satoshilabs/slips/blob/master/slip-0010.md#test-vector-2-for-ed25519
+  const ed25519 = [
+    {
+      seed: '000102030405060708090a0b0c0d0e0f',
+      vectors: [
+        {
+          chain: 'm',
+          private:
+            '2b4be7f19ee27bbf30c667b642d5f4aa69fd169872f8fc3059c08ebae2eb19e7',
+          public:
+            '00a4b2856bfec510abab89753fac1ac0e1112364e7d250545963f135f2a33188ed'
+        },
+        {
+          chain: "m/0'",
+          private:
+            '68e0fe46dfb67e368c75379acec591dad19df3cde26e63b93a8e704f1dade7a3',
+          public:
+            '008c8a13df77a28f3445213a0f432fde644acaa215fc72dcdf300d5efaa85d350c'
+        },
+        {
+          chain: "m/0'/1'",
+          private:
+            'b1d0bad404bf35da785a64ca1ac54b2617211d2777696fbffaf208f746ae84f2',
+          public:
+            '001932a5270f335bed617d5b935c80aedb1a35bd9fc1e31acafd5372c30f5c1187'
+        },
+        {
+          chain: "m/0'/1'/2'",
+          private:
+            '92a5b23c0b8a99e37d07df3fb9966917f5d06e02ddbd909c7e184371463e9fc9',
+          public:
+            '00ae98736566d30ed0e9d2f4486a64bc95740d89c7db33f52121f8ea8f76ff0fc1'
+        },
+        {
+          chain: "m/0'/1'/2'/2'",
+          private:
+            '30d1dc7e5fc04c31219ab25a27ae00b50f6fd66622f6e9c913253d6511d1e662',
+          public:
+            '008abae2d66361c879b900d204ad2cc4984fa2aa344dd7ddc46007329ac76c429c'
+        },
+        {
+          chain: "m/0'/1'/2'/2'/1000000000'",
+          private:
+            '8f94d394a8e8fd6b1bc2f3f49f5c47e385281d5c17e65324b0f62483e37e8793',
+          public:
+            '003c24da049451555d51a7014a37337aa4e12d41e485abccfa46b47dfb2af54b7a'
+        }
+      ]
+    },
+    {
+      seed:
+        'fffcf9f6f3f0edeae7e4e1dedbd8d5d2cfccc9c6c3c0bdbab7b4b1aeaba8a5a29f9c999693908d8a8784817e7b7875726f6c696663605d5a5754514e4b484542',
+      vectors: [
+        {
+          chain: 'm',
+          private:
+            '171cb88b1b3c1db25add599712e36245d75bc65a1a5c9e18d76f9f2b1eab4012',
+          public:
+            '008fe9693f8fa62a4305a140b9764c5ee01e455963744fe18204b4fb948249308a'
+        },
+        {
+          chain: "m/0'",
+          private:
+            '1559eb2bbec5790b0c65d8693e4d0875b1747f4970ae8b650486ed7470845635',
+          public:
+            '0086fab68dcb57aa196c77c5f264f215a112c22a912c10d123b0d03c3c28ef1037'
+        },
+        {
+          chain: "m/0'/2147483647'",
+          private:
+            'ea4f5bfe8694d8bb74b7b59404632fd5968b774ed545e810de9c32a4fb4192f4',
+          public:
+            '005ba3b9ac6e90e83effcd25ac4e58a1365a9e35a3d3ae5eb07b9e4d90bcf7506d'
+        },
+        {
+          chain: "m/0'/2147483647'/1'",
+          private:
+            '3757c7577170179c7868353ada796c839135b3d30554bbb74a4b1e4a5a58505c',
+          public:
+            '002e66aa57069c86cc18249aecf5cb5a9cebbfd6fadeab056254763874a9352b45'
+        },
+        {
+          chain: "m/0'/2147483647'/1'/2147483646'",
+          private:
+            '5837736c89570de861ebc173b1086da4f505d4adb387c6a1b1342d5e4ac9ec72',
+          public:
+            '00e33c0f7d81d843c572275f287498e8d408654fdf0d1e065b84e2e6f157aab09b'
+        },
+        {
+          chain: "m/0'/2147483647'/1'/2147483646'/2'",
+          private:
+            '551d333177df541ad876a60ea71f00447931c0a9da16f227c11ea080d7391b8d',
+          public:
+            '0047150c75db263559a70d5778bf36abbab30fb061ad69f69ece61a72b0cfa4fc0'
+        }
+      ]
+    }
+  ];
   it('should generate key from mnemonic', () => {
     const key = Ed25519HDKey.new();
 
@@ -23,26 +116,17 @@ describe('Ed25519HDKey', () => {
     expect(key0.accountHex()).eq(recoveredKey0.accountHex());
   });
 
-  it('Ed25519HDKey - vector1 - m', () => {
-    const key = new Ed25519HDKey(vector1);
-    const mKey = key.derive('m');
+  ed25519.forEach(({ seed, vectors }) => {
+    describe(`Test seed ${seed}`, () => {
+      vectors.forEach(({ chain, private: privateKey, public: publicKey }) => {
+        it(`should generate correct key pair for ${chain}`, () => {
+          const hdKey = new Ed25519HDKey(decodeBase16(seed));
+          const pair = hdKey.derive(chain);
 
-    expect(encodeBase16(mKey.privateKey)).eq(
-      '2b4be7f19ee27bbf30c667b642d5f4aa69fd169872f8fc3059c08ebae2eb19e7'
-    );
-    expect('00' + encodeBase16(mKey.publicKey.value())).eq(
-      '00a4b2856bfec510abab89753fac1ac0e1112364e7d250545963f135f2a33188ed'
-    );
-  });
-  it('Ed25519HDKey - vector2 - m', () => {
-    const key = new Ed25519HDKey(vector2);
-    const mKey = key.derive('m');
-
-    expect(encodeBase16(mKey.privateKey)).eq(
-      '171cb88b1b3c1db25add599712e36245d75bc65a1a5c9e18d76f9f2b1eab4012'
-    );
-    expect('00' + encodeBase16(mKey.publicKey.value())).eq(
-      '008fe9693f8fa62a4305a140b9764c5ee01e455963744fe18204b4fb948249308a'
-    );
+          expect(encodeBase16(pair.privateKey)).eq(privateKey);
+          expect('00' + encodeBase16(pair.publicKey.data)).eq(publicKey);
+        });
+      });
+    });
   });
 });
--- src/lib/CasperHDKeys/Secp256K1HDKey.test.ts
+++ src/lib/CasperHDKeys/Secp256K1HDKey.test.ts
@@ -1,22 +1,132 @@
 import { expect } from 'chai';
 import { Secp256K1HDKey } from './Secp256K1HDKey';
+import { decodeBase16, encodeBase16 } from '../Conversions';
 
 describe('Secp256K1HDKey', () => {
+  // https://github.com/satoshilabs/slips/blob/master/slip-0010.md#test-vector-1-for-secp256k1
+  // https://github.com/satoshilabs/slips/blob/master/slip-0010.md#test-vector-2-for-secp256k1
+  const secp256k1 = [
+    {
+      seed: '000102030405060708090a0b0c0d0e0f',
+      vectors: [
+        {
+          chain: 'm',
+          private:
+            'e8f32e723decf4051aefac8e2c93c9c5b214313817cdb01a1494b917c8436b35',
+          public:
+            '0339a36013301597daef41fbe593a02cc513d0b55527ec2df1050e2e8ff49c85c2'
+        },
+        {
+          chain: "m/0'",
+          private:
+            'edb2e14f9ee77d26dd93b4ecede8d16ed408ce149b6cd80b0715a2d911a0afea',
+          public:
+            '035a784662a4a20a65bf6aab9ae98a6c068a81c52e4b032c0fb5400c706cfccc56'
+        },
+        {
+          chain: "m/0'/1",
+          private:
+            '3c6cb8d0f6a264c91ea8b5030fadaa8e538b020f0a387421a12de9319dc93368',
+          public:
+            '03501e454bf00751f24b1b489aa925215d66af2234e3891c3b21a52bedb3cd711c'
+        },
+        {
+          chain: "m/0'/1/2'",
+          private:
+            'cbce0d719ecf7431d88e6a89fa1483e02e35092af60c042b1df2ff59fa424dca',
+          public:
+            '0357bfe1e341d01c69fe5654309956cbea516822fba8a601743a012a7896ee8dc2'
+        },
+        {
+          chain: "m/0'/1/2'/2",
+          private:
+            '0f479245fb19a38a1954c5c7c0ebab2f9bdfd96a17563ef28a6a4b1a2a764ef4',
+          public:
+            '02e8445082a72f29b75ca48748a914df60622a609cacfce8ed0e35804560741d29'
+        },
+        {
+          chain: "m/0'/1/2'/2/1000000000",
+          private:
+            '471b76e389e528d6de6d816857e012c5455051cad6660850e58372a6c3e6e7c8',
+          public:
+            '022a471424da5e657499d1ff51cb43c47481a03b1e77f951fe64cec9f5a48f7011'
+        }
+      ]
+    },
+    {
+      seed:
+        'fffcf9f6f3f0edeae7e4e1dedbd8d5d2cfccc9c6c3c0bdbab7b4b1aeaba8a5a29f9c999693908d8a8784817e7b7875726f6c696663605d5a5754514e4b484542',
+      vectors: [
+        {
+          chain: 'm',
+          private:
+            '4b03d6fc340455b363f51020ad3ecca4f0850280cf436c70c727923f6db46c3e',
+          public:
+            '03cbcaa9c98c877a26977d00825c956a238e8dddfbd322cce4f74b0b5bd6ace4a7'
+        },
+        {
+          chain: 'm/0',
+          private:
+            'abe74a98f6c7eabee0428f53798f0ab8aa1bd37873999041703c742f15ac7e1e',
+          public:
+            '02fc9e5af0ac8d9b3cecfe2a888e2117ba3d089d8585886c9c826b6b22a98d12ea'
+        },
+        {
+          chain: "m/0/2147483647'",
+          private:
+            '877c779ad9687164e9c2f4f0f4ff0340814392330693ce95a58fe18fd52e6e93',
+          public:
+            '03c01e7425647bdefa82b12d9bad5e3e6865bee0502694b94ca58b666abc0a5c3b'
+        },
+        {
+          chain: "m/0/2147483647'/1",
+          private:
+            '704addf544a06e5ee4bea37098463c23613da32020d604506da8c0518e1da4b7',
+          public:
+            '03a7d1d856deb74c508e05031f9895dab54626251b3806e16b4bd12e781a7df5b9'
+        },
+        {
+          chain: "m/0/2147483647'/1/2147483646'",
+          private:
+            'f1c7c871a54a804afe328b4c83a1c33b8e5ff48f5087273f04efa83b247d6a2d',
+          public:
+            '02d2b36900396c9282fa14628566582f206a5dd0bcc8d5e892611806cafb0301f0'
+        },
+        {
+          chain: "m/0/2147483647'/1/2147483646'/2",
+          private:
+            'bb7d39bdb83ecf58f2fd82b6d918341cbef428661ef01ab97c28a4842125ac23',
+          public:
+            '024d902e1a2fc7a8755ab5b694c575fce742c48d9ff192e63df5193e4c7afe1f9c'
+        }
+      ]
+    }
+  ];
   it('should generate key from mnemonic', () => {
     const key = Secp256K1HDKey.new();
 
     const key0 = key.deriveChild(1);
 
     const mn = key.mnemonic;
-    console.log(mn);
-    console.log(key.deriveChild(0).accountHex());
-    console.log(key.deriveChild(1).accountHex());
-    console.log(key.deriveChild(2).accountHex());
 
     const recoveredKey = Secp256K1HDKey.fromMnemonic(mn);
 
     const recoveredKey0 = recoveredKey.deriveChild(1);
 
     expect(key0.accountHex()).eq(recoveredKey0.accountHex());
+  });
+
+  secp256k1.forEach(({ seed, vectors }) => {
+    describe(`Test seed ${seed}`, () => {
+      vectors.forEach(({ chain, private: privateKey, public: publicKey }) => {
+        it(`should generate correct key pair for ${chain}`, () => {
+          const hdKey = new Secp256K1HDKey(decodeBase16(seed));
+          const pair = hdKey.derive(chain);
+
+          expect(encodeBase16(pair.privateKey)).eq(privateKey);
+          expect(encodeBase16(pair.publicKey.data)).eq(publicKey);
+        });
+      });
+    });
   });
 });
