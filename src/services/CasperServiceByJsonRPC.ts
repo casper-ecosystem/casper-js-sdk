6239375bd09c3b3453816cb8d9f42384c18207d5
gyroflaw <83718263+gyroflaw@users.noreply.github.com>
2023-07-07 16:06:21 +0900

    fix: allow account_put_deploy to throw json rpc error

--- src/services/CasperServiceByJsonRPC.ts
+++ src/services/CasperServiceByJsonRPC.ts
@@ -15,6 +15,8 @@ import ProviderTransport, {
   SafeEventEmitterProvider
 } from './ProviderTransport';
 
+export { JSONRPCError } from '@open-rpc/client-js';
+
 /** RPC request props interface */
 interface RpcRequestProps {
   timeout?: number;
@@ -677,17 +679,13 @@ export class CasperServiceByJsonRPC {
 
     // TODO: Check if the deploy is signed properly
 
-    try {
-      return await this.client.request(
-        {
-          method: 'account_put_deploy',
-          params: DeployUtil.deployToJson(signedDeploy)
-        },
-        props?.timeout
-      );
-    } catch (e) {
-      return e;
-    }
+    return await this.client.request(
+      {
+        method: 'account_put_deploy',
+        params: DeployUtil.deployToJson(signedDeploy)
+      },
+      props?.timeout
+    );
   }
 
   /**
